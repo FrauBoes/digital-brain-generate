a7b4c8079d809b04bc069475f278a0af38acdc78
FrauBoes <28864823+FrauBoes@users.noreply.github.com>
2025-02-07 10:44:44 +0100

    read nfc tag on windows and add instructions

--- read_uuid_from_nfc_tag.py
+++ read_uuid_from_nfc_tag.py
@@ -1,6 +1,29 @@
 from smartcard.System import readers
 from smartcard.util import toHexString
 
+'''
+Read the UID from a NFC tag
+
+Equipment needed: NFC tag, USB NFC reader
+
+MacOS
+1. Plug in reader and install drivers from manufacturer's website: https://www.akasa.co.uk/search.php?seed=AK-CR-15BK
+2. Install middleware to enable card readers, for example https://pcsclite.apdu.fr/ or pcsc.
+3. Run `pcsctest` to check reader is recognized by the computer
+4. Put tag on reader, a green light should light up.
+5. Install all requirements by running `pip install -r requirements.txt` inside this project
+6. Run this script by running `python read_uuid_from_nfc_tag.py`
+
+Windows
+1. Plug in reader. It should be recognized automatically and no drivers need to be installed.
+2. Check reader is listed:
+    2.1. In the Device Manager under Smart card readers
+    2.2. In the Command Prompt (run as admin) by running `sc query scardvr`
+3. Put tag on reader, a green light should light up.
+4. Install all requirements by running `pip install -r requirements.txt` inside this project
+5. Run this script by running `python read_uuid_from_nfc_tag.py`
+'''
+
 def read_nfc_tag():
     # Get available readers
     r = readers()
@@ -12,9 +35,11 @@ def read_nfc_tag():
         print("No smart card readers found.")
         return
 
-    # Use the second reader, which is the NFC reader ("Generic EMV Smartcard Reader 01")
-    # The first reader is the smart card reader ("Generic EMV Smartcard Reader 02")
-    reader = r[1]
+    '''
+    Use the first reader, which is the NFC reader ("Generic EMV Smartcard Reader 0")
+    The second reader is the smart card reader ("Generic EMV Smartcard Reader 01")
+    '''
+    reader = r[0]
     print(f"Using reader: {reader}")
     
     connection = reader.createConnection()

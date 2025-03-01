05759bf08d49a17170f590fd61175aada06198ca
FrauBoes <28864823+FrauBoes@users.noreply.github.com>
2025-03-01 10:55:48 +0100

    Add audio generation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import eeg.collect_filtered_data
 import eeg.interpolate_data
+import processing.create_audio
 import processing.create_quadrant_animation
 import processing.create_radar_animation
 import processing.create_radar_animation
@@ -9,6 +10,7 @@ FILE_DATA_FILTERED = 'data/data_filtered.csv'
 FILE_DATA_INTERPOLATED = 'data/data_interpolated.csv'
 FILE_RADAR_ANIMATION = 'artifacts/radar_animation.mp4'
 FILE_QUADRANT_ANIMATION = 'artifacts/quadrant_animation.mp4'
+FILE_AUDIO = 'artifacts/audio.mid'
 
 eeg.collect_filtered_data.collect_filtered_data()
 time.sleep(30)
@@ -16,3 +18,4 @@ eeg.interpolate_data.interpolate_to_16_columns(FILE_DATA_FILTERED, FILE_DATA_INT
 
 processing.create_radar_animation.run_radar_animation(FILE_DATA_INTERPOLATED, FILE_RADAR_ANIMATION)
 processing.create_quadrant_animation.run_quadrant_animation(FILE_DATA_INTERPOLATED, FILE_QUADRANT_ANIMATION)
+processing.create_audio.create_audio(FILE_DATA_FILTERED,FILE_AUDIO)

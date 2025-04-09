b81e27ba9f72b71be0c340e2a5c63a3691517f91
FrauBoes <28864823+FrauBoes@users.noreply.github.com>
2025-04-09 11:02:54 +0200

    Add new audio generation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import eeg.collect_filtered_data
 import eeg.interpolate_data
 import nfc_tag.read_uuid_from_nfc_tag
 import processing.create_audio
+import processing.create_audio_new
 import processing.create_quadrant_animation
 import processing.create_radar_animation
 import processing.create_radar_animation
@@ -13,6 +14,7 @@ FILE_DATA_INTERPOLATED = 'data/data_interpolated.csv'
 FILE_RADAR_ANIMATION = 'artifacts/radar_animation.mp4'
 FILE_QUADRANT_ANIMATION = 'artifacts/quadrant_animation.mp4'
 FILE_AUDIO = 'artifacts/audio.mid'
+FILE_AUDIO_NEW = 'artifacts/audio-new.mid'
 
 # Read and preprocess data
 eeg.collect_filtered_data.collect_filtered_data()
@@ -23,6 +25,7 @@ eeg.interpolate_data.interpolate_to_16_columns(FILE_DATA_FILTERED, FILE_DATA_INT
 processing.create_radar_animation.run_radar_animation(FILE_DATA_INTERPOLATED, FILE_RADAR_ANIMATION)
 processing.create_quadrant_animation.run_quadrant_animation(FILE_DATA_INTERPOLATED, FILE_QUADRANT_ANIMATION)
 processing.create_audio.create_audio(FILE_DATA_FILTERED,FILE_AUDIO)
+processing.create_audio_new.create_midi(FILE_DATA_FILTERED, FILE_AUDIO_NEW)
 
 # Read uuid
 uuid = nfc_tag.read_uuid_from_nfc_tag.get_uuid()
